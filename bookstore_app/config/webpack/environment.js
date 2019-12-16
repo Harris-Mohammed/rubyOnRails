de444b3cac47cdb3fbd49000dd26e59452547768
b6018020@my.shu.ac.uk <student@advsysarchs2019.localdomain>
2019-12-16 17:21:35 +0000

    created contact form and route config

--- bookstore_app/config/webpack/environment.js
+++ bookstore_app/config/webpack/environment.js
@@ -1,3 +1,10 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = require('webpack')
+environment.plugins.append('provide', new webpack.ProvidePlugin({
+    $: 'jquery',
+    jquery: 'jquery',
+    popper: ['popper.js', 'default']
+}))
+
 module.exports = environment
